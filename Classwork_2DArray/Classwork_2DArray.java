937a4856d49759676a78c76e1f9bff6abe8ce02e
Bearindigo105 <m.muthusubhash@gmail.com>
2023-11-02 18:47:29 -0400

    modified:   Classwork_2DArray/Classwork_2DArray.java

--- Classwork_2DArray/Classwork_2DArray.java
+++ Classwork_2DArray/Classwork_2DArray.java
@@ -7,25 +7,29 @@ public class Classwork_2DArray {
     public static void main(String[] args) {
         
         int[] oneDarr = new int[50 + (int)(Math.random() * 51)];
-        int[][] twoDarr = new int[Math.ceil(oneDarr.length / 7.0)][7];
+        int[][] twoDarr = new int[(int)Math.ceil(oneDarr.length / 7.0)][7];
 
         for (int i = 0; i < oneDarr.length; i++) {
             oneDarr[i] = (int)(Math.random() * 100) + 1;
         }
-
+        
         for (int i = 0; i < twoDarr.length; i++) {
             for (int j = 0; j < twoDarr[i].length; j++) {
                 twoDarr[i][j] = 0;
             }
         }
-
         for (int i = 0; i < oneDarr.length; i++) {
-
+            
             if(i % 7 == 0){
                 System.out.println(); // newline
             }
             
             System.out.print(oneDarr[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0; i < oneDarr.length; i++) {
+            
+            
         }
 
         System.out.println(); // newline
