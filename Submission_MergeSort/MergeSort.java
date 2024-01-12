b5b32a56c059d782a92dc0516414eb09c2d0db12
Bearindigo105 <m.muthusubhash@gmail.com>
2024-01-12 08:26:47 -0500

    new file:   Submission_MergeSort/MergeSort.java 	deleted:    Submission_TestQuestions/MCQuestion.java 	deleted:    Submission_TestQuestions/MultipleSelect.java 	deleted:    Submission_TestQuestions/Muthu Subhash Muthu_chess_output.txt 	deleted:    Submission_TestQuestions/TestMain.java 	deleted:    Submission_TestQuestions/TestQuestion.java 	deleted:    Submission_TestQuestions/TestQuestionUnitTests.java 	deleted:    Submission_TestQuestions/lib/hamcrest-core-1.3.jar 	deleted:    Submission_TestQuestions/lib/junit-4.13.2.jar

---
+++ Submission_MergeSort/MergeSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args){
+		int[] start = new int[10];
+		for(int i = 0; i < start.length; i++)
+		{
+			start[i] = (int)(Math.random()*20 + 1);
+		}
+		int[] sorted = Arrays.copyOf(start,start.length);
+		Arrays.sort(sorted);
+		mergesort(start);
+		if(Arrays.equals(start, sorted))
+			System.out.print("correctly sorted");
+		else
+			System.out.print("not properly sorted");
+	}
+	
+	/*
+	 * implement the mergesort method.  It should be a recursive
+	 * implementation of the mergesort algorithm we spoke about
+	 * in class.  If you run this main method it will tell you 
+	 * if your algorithm properly sorted the array.  You do not 
+	 * need to add any code to the main method.
+	 */
+	
+	public static void mergesort(int[] arr){
+		int[] arrL = Arrays.copyOfRange(arr, 0, arr.length/2);
+		int[] arrR = Arrays.copyOfRange(arr, arr.length/2, arr.length);
+		System.out.println(arrL[arrL.length] + " " + arrR[0]);
+	}
+
+}
