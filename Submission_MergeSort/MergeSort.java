4e01559b346e37e44d18c399b8b0a5462f8208f1
Bearindigo105 <m.muthusubhash@gmail.com>
2024-01-15 22:45:37 -0500

    modified:   Submission_MergeSort/MergeSort.java

--- Submission_MergeSort/MergeSort.java
+++ Submission_MergeSort/MergeSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class MergeSort {
 
 	public static void main(String[] args) {
-		int[] start = new int[8];
+		int[] start = new int[10];
 		for(int i = 0; i < start.length; i++)
 		{
 		start[i] = (int)(Math.random()*20 + 1);
@@ -19,28 +19,21 @@ public class MergeSort {
 		System.out.println(Arrays.toString(sorted));
 	}
 
-	/*
-	 * implement the mergesort method. It should be a recursive
-	 * implementation of the mergesort algorithm we spoke about
-	 * in class. If you run this main method it will tell you
-	 * if your algorithm properly sorted the array. You do not
-	 * need to add any code to the main method.
+	/**
+	 * @param arr
+	 * @apiNote sorts arr from least to greatest.
 	 */
-
 	public static void mergesort(int[] arr) {
-		System.out.println();
-		System.out.println(Arrays.toString(arr));
-
 		if (arr.length > 1) {
 			int[] arrL = Arrays.copyOfRange(arr, 0, arr.length / 2);
 			int[] arrR = Arrays.copyOfRange(arr, arr.length / 2, arr.length);
+
 			mergesort(arrL);
 			mergesort(arrR);
 
 			int i = 0;
 			int j = 0;
 			int k = 0;
-
 			while (i < arrL.length && j < arrR.length) {
 				if (arrL[i] <= arrR[j]) {
 					arr[k] = arrL[i];
@@ -51,20 +44,16 @@ public class MergeSort {
 				}
 				k++;
 			}
-
 			while (i < arrL.length) {
 				arr[k] = arrL[i];
 				i++;
 				k++;
 			}
-
 			while (j < arrR.length) {
 				arr[k] = arrR[j];
 				j++;
 				k++;
 			}
 		}
-		System.out.println(Arrays.toString(arr));
 	}
-
 }
